c89b1243265d1fc6895c8b7e16549fffa32a530e
svigneshkumar <s.vigneshkumar@hotmail.com>
2021-01-26 09:21:14 +0530

    added youtube search command

--- voice.py
+++ voice.py
@@ -33,7 +33,7 @@ def record_audio(ask = False):
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
-            #print(voice_data)
+            print("You :",voice_data)
         except sr.UnknownValueError:
             cruise_speak("Sorry, I don't understand")
         except sr.RequestError:
@@ -46,10 +46,11 @@ def cruise_speak(audio_string):
     audio_file = 'audio-' + str(r) + '.mp3'
     tts.save(audio_file)
     playsound.playsound(audio_file)
-    print(audio_string)
+    print("Bot :",audio_string)
     os.remove(audio_file)  
     
 def respond(voice_data):
+    voice_data = voice_data.lower()
     if "what is your name" in voice_data:
         cruise_speak("My name is Cruise") 
     if "what is the time now" in voice_data:
@@ -64,6 +65,12 @@ def respond(voice_data):
         url = 'https://google.nl/maps/place/' + location + '/&amp;'
         webbrowser.get().open(url)
         cruise_speak('Here is the location of '+ location)
+    if "youtube" in voice_data:
+        youtube_search = record_audio("What do you want to search for?")
+        search_term = youtube_search.split("for")[-1]
+        url = f"https://www.youtube.com/results?search_query={search_term}"
+        webbrowser.get().open(url)
+        cruise_speak(f'Here is what I found for {search_term} on youtube')
     if "exit" in voice_data:
         cruise_speak("Thank You")
         exit()
